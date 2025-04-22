3fc0633af11d7f74ff3616bf63ba637167985c83
daniel duda <danielosvaldoduda93@gmail.com>
2025-04-22 00:03:37 -0300

    añadimos botones, formulario

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,18 @@ function toggleMenu() {
     var navLinks = document.getElementById("nav-links");
     navLinks.classList.toggle("show"); // Agrega o quita la clase 'show'
 }
+
+
+// Script para que funcione el envío del formulario a través del servicio de EmailJS
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_yphksjj', 'template_0lhiiy8', this)
+      .then(function() {
+        alert('Mensaje enviado con éxito 🎉');
+      }, function(error) {
+        console.error('Error:', error);
+        alert('Hubo un problema 😢');
+      });
+  });
